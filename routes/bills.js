bc87f43d613cf20c52f9629452a7b31ad7974552
Srikanth Reddy Gubballi <gubballi.s@northeastern.edu>
2020-01-26 02:33:26 -0500

    routes for bills
    add controller, routes and queries to fetch details from the database

---
+++ routes/bills.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+const BillController = require("../controllers/bills");
+
+// Get all bill
+router.get('/', BillController.get_all_bills);
+
+// Get a specific bill
+router.get('/:id', BillController.get_bills);
+
+// Add a new bill  
+router.post('/', BillController.post_bills);
+
+// Update bill details
+router.put('/', BillController.update_bills);
+
+
+module.exports = router;

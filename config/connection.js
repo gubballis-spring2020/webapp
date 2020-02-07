7300cbbe3777b601f4f13e3b1a14bf43d37775ca
Srikanth Reddy Gubballi <gubballi.s@northeastern.edu>
2020-02-06 23:18:42 -0500

    update code
    1) use sequelize
    2) change the code to use sequelize - add models and update queries

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,39 @@
 const mysql = require('mysql');
 const dbConfig = require('./db.config');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-var mysqlConnection = mysql.createConnection({
+// var mysqlConnection = mysql.createConnection({
 
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
+//     // host: process.env.DB_HOST,
+//     // port: process.env.DB_PORT,
+//     // user: process.env.DB_USER,
+//     // password: process.env.DB_PASSWORD,
+//     // database: process.env.DB_NAME,
 
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
+//     host: dbConfig.HOST,
+//     user: dbConfig.USER,
+//     password: dbConfig.PASSWORD,
+//     database: dbConfig.DB
     
-});
+// });
 
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log("Connected");
-    else
-        console.log("Connection error");
-});
+mysqlConnection =  new Sequelize(dbConfig.DB , dbConfig.USER, dbConfig.PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+  });
+
+  mysqlConnection.authenticate()
+                 .then(() => console.log('Connected'))
+                 .catch(err => console.log(err))
 
 module.exports = mysqlConnection;

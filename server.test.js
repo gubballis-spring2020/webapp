93b8a9ecdf9eb795e99c022a84ff633c8a56a5d9
Srikanth Reddy Gubballi <gubballi.s@northeastern.edu>
2020-03-05 00:58:40 -0600

    check circleci run for test cases

--- server.test.js
+++ server.test.js
@@ -4,6 +4,22 @@ const request = supertest(app)
 
 // to test the post endpoint for user
 describe('Post Endpoints', () => {
+  it('should create a new user', async () => {
+    const res = await supertest(app)
+      .post('/v1/user')
+      .send({
+        first_name: "Jane",
+        last_name: "Doe",
+        password: "Cloud007",
+        email_address: "jane.doe@example.com"
+      })
+    expect(res.statusCode).toEqual(201)
+    // expect(res.body).toHaveProperty('post')
+  })
+});
+
+// to test the post endpoint for user
+describe('Post Endpoints', () => {
   it('should throw an error when create a new user', async () => {
     const res = await supertest(app)
       .post('/v1/user')
@@ -32,6 +48,39 @@ describe('Put Endpoints', () => {
   })
 });
 
+// to test get endpoint for user
+describe('Get Endpoints', () => {
+  it('should throw an error when retrieving a user', async () => {
+    const res = await supertest(app)
+      .get('/v1/user')
+      .auth("jane.doe@example.com","Cloud007")
+    expect(res.statusCode).toEqual(200)
+    // expect(res.body).toHaveProperty('post')
+  })
+});
+
+// to test the post endpoint for bill
+describe('Post Endpoints', () => {
+  it('should create a new bill', async () => {
+    const res = await supertest(app)
+      .post('/v1/bill')
+      .auth("jane.doe@example.com","Cloud007")
+      .send({
+        vendor: "Northeastern University",
+        bill_date: "2020-01-06",
+        due_date: "2020-01-12",
+        amount_due: 7000.51,
+        categories: [
+          "college",
+          "tuition",
+          "spring2020"
+        ],
+        paymentStatus: "paid"
+      })
+    expect(res.statusCode).toEqual(201)
+    // expect(res.body).toHaveProperty('post')
+  })
+});
 
 // to test the get endpoint for bill
 describe('Get Endpoints', () => {

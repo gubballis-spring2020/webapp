959da2d17a4321fcd4d526e14847cc2165a625b3
Srikanth Reddy Gubballi <gubballi.s@northeastern.edu>
2020-03-10 21:53:53 -0400

    changes to hook scripts

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -3,18 +3,21 @@
 # update the permission and ownership of WAR file in the tomcat webapps directory
 echo "#CSYE6225: doing after install"
 pwd
-cd /home/centos/
-sudo mkdir ccwebapps
-#move tar.gz to ccwebapps
-sudo mv webapp.tar.gz ccwebapps/
-#remove from ccwebapps
-sudo rm -rf webapp
-#unzip in ccwebapps
-cd ccwebapps
-sudo tar xzvf webapp.tar.gz
-sudo rm -rf webapp.tar.gz
-
-cd webapp
-pwd
+cd /home/ubuntu/
 sudo npm install
 sudo npm install pm2 -g
+
+# sudo mkdir ccwebapps
+# #move tar.gz to ccwebapps
+# sudo mv webapp.zip ccwebapps/
+# #remove from ccwebapps
+# sudo rm -rf webapp
+# #unzip in ccwebapps
+# cd ccwebapps
+# sudo tar xzvf webapp.tar.gz
+# sudo rm -rf webapp.tar.gz
+
+# cd webapp
+# pwd
+# sudo npm install
+# sudo npm install pm2 -g

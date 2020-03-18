b064bd40aa1c37d4a3b9a8023136cbfcf882423b
Srikanth Reddy Gubballi <gubballi.s@northeastern.edu>
2020-03-18 01:48:44 -0400

    changes for cloudwatch config

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -4,6 +4,10 @@
 echo "#CSYE6225: doing after install"
 pwd
 cd /home/ubuntu/webapp
+sudo mkdir logs
+cd logs
+sudo touch webapp.log
+cd ../
 sudo cp -rf amazon-cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-config.json
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-config.json -s
 sudo npm install
